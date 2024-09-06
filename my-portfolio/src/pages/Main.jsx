77e04dab682d3d94d66354e75baf4290693a06fe
Lee Anne Angeles <leeangeles2@gmail.com>
2024-09-06 13:53:47 +0800

    chore: minor changes in style

--- my-portfolio/src/pages/Main.jsx
+++ my-portfolio/src/pages/Main.jsx
@@ -33,10 +33,10 @@ const Main = () => {
                         loop
                     />
                 </div>
-                <p className="p-2 md:text-2xl sm:text-1xl font-medium text-gray-400">
+                <p className="px-10 py-2 md:text-2xl sm:text-1xl font-medium text-gray-400">
                     Currently a Third-year Computer Engineering student at the Polytechnic University of the Philippines.
                 </p>
-                <button type="button" class="mt-4 text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Let's Connect</button>
+                <button type="button" class="mt-4 text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2">Let's Connect</button>
 
             </div>
         </div>
