949c1e7cc22f66573515c16c52cf2fb613b286f6
Lee Anne Angeles <leeangeles2@gmail.com>
2024-10-07 02:22:50 +0800

    feat: minor changes in styles on education page

--- my-portfolio/src/pages/Education.jsx
+++ my-portfolio/src/pages/Education.jsx
@@ -62,7 +62,7 @@ const Education = () => {
     <div className="min-h-screen max-w-[1200px] my-20 mx-auto bg-[#000300] text-white">
       <div className="mx-4 md:mx-6 sm:mx-8 lg:mx-10 bg-[#1a1a1a] my-20 rounded-lg">
         <div className="flex items-center mx-10 pt-10">
-          <LinearGradient gradient={['to left', '#ff68f0 , #89CFF0']} className="text-xl sm:text-2xl md:text-[20px] lg:text-[25px] font-bold text-[#89CFF0] text-center">
+          <LinearGradient gradient={['to left', '#ff68f0 , #89CFF0']} className="text-2xl sm:text-3xl md:text-[25px] lg:text-[30px] font-bold text-[#89CFF0] text-center">
             Education
           </LinearGradient>
           <hr className="flex border-t-5 border-[#89CFF0] ml-4 w-[150px] md:w-[200px]" />
@@ -78,11 +78,11 @@ const Education = () => {
             />
           </div>
 
-          <div className="w-full p-5 md:w-2/3 flex flex-col justify-center items-center md:items-start slide-in-right">
-            <h2 className="mb-4 text-[#89CFF0] text-lg sm:text-xl md:text-2xl font-semibold">
+          <div className="w-full p-5 md:w-2/3 flex flex-col justify-center md:items-start slide-in-right">
+            <h2 className="mb-4 text-[#89CFF0] text-xl font-semibold tracking-wide">
               Basic Qualification and Certifications
             </h2>
-            <p className="text-gray-400 text-sm md:text-md lg:text-lg font-regular mb-2">
+            <p className="text-gray-400 text-md font-regular mb-2">
               Welcome to my Education page! 🙋‍♀️ Here, you'll find a summary of my academic qualifications and the journey that has shaped my career. Education is central to my growth, and I’m dedicated to continuous learning and staying updated with the latest advancements in technology. This page showcases the key milestones of my educational journey and my commitment to lifelong learning.
             </p>
           </div>
@@ -98,7 +98,7 @@ const Education = () => {
             <VerticalTimelineElement
               key={index}
               className="vertical-timeline-element"
-              date={<span style={{ color: lineColor, fontSize: '12px', textAlign: 'center', display: 'block' }}>{element.date}</span>}
+              date={<span style={{ color: lineColor, fontSize: '14px', textAlign: 'center', display: 'block' }}>{element.date}</span>}
               contentStyle={{
                 boxShadow: `0px 0px 6px ${lineColor}`,
                 backgroundColor: "transparent",
@@ -120,13 +120,13 @@ const Education = () => {
                 <img 
                   src={element.logo} 
                   alt={`${element.school} Logo`} 
-                  className="w-8 h-8 md:w-10 md:h-10 hover:float-effect"
+                  className="w-10 h-10 md:w-10 md:h-10"
                 />
               }
             >
-              <h3 className="text-sm sm:text-md font-semibold">{element.school}</h3>
-              <h4 className="text-xs sm:text-sm text-[#89CFF0]">{element.level}</h4>
-              <h5 className="text-xs sm:text-sm text-gray-300 mt-1">{element.location}</h5>
+              <h3 className="text-md font-semibold tracking-wide">{element.school}</h3>
+              <h4 className="text-md text-[#89CFF0] tracking-wide">{element.level}</h4>
+              <h5 className="text-sm text-gray-300 mt-1 tracking-wide">{element.location}</h5>
             </VerticalTimelineElement>
           ))}
         </VerticalTimeline>
